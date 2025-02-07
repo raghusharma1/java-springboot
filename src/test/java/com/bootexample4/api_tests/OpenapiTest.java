81c661c7d8ad7fbec51d6008cfff0e7425a6b7d3
raghusharma1 <raghu1sharma77@gmail.com>
2025-02-07 02:53:12 +0000

    API test (Using API Spec) generated by RoostGPT
    Using AI Model gpt-4-turbo

---
+++ src/test/java/com/bootexample4/api_tests/OpenapiTest.java
@@ -0,0 +1,25 @@
+
+  package com.bootexample4.api_tests;
+
+  import com.intuit.karate.Results;
+  import com.intuit.karate.Runner;
+  // import com.intuit.karate.http.HttpServer;
+  // import com.intuit.karate.http.ServerConfig;
+  import org.junit.jupiter.api.Test;
+
+  import static org.junit.jupiter.api.Assertions.assertEquals;
+
+  class OpenapiTest {
+
+      @Test
+      void testAll() {
+          String apiHostServer = System.getenv().getOrDefault("nnnn_URL_BASE", "http://localhost:4010");
+String nnnnauthtoken = System.getenv().getOrDefault("nnnn_AUTH_TOKEN", "dummy_nnnn_AUTH_TOKEN");
+          Results results = Runner.path("src/test/java/com/bootexample4/api_tests/Openapi")
+                  .systemProperty("nnnn_URL_BASE", apiHostServer)
+.systemProperty("nnnn_AUTH_TOKEN", nnnnauthtoken)
+                  .reportDir("testReport").parallel(1);
+          assertEquals(0, results.getFailCount(), results.getErrorMessages());
+      }
+
+  }
